72c48b8e6170db12f2f109ba7660b903f0445157
Smith Gulati <smith@central.tech>
2024-07-25 07:30:09 +0000

    commit test git

--- includes/constants.js
+++ includes/constants.js
@@ -6,9 +6,12 @@ const _RAW_PROJECT_ID_ = 'central-cto-cds-data-hub-prd';
 const _MART_PROJECT_ID_ = 'cto-cds-datamart-hub-prod';
 const _PRESENTATION_PROJECT_ID_ = 'cto-cds-presentation-hub-prd';
 
+const _GENDER_THRESHOLD_ = 0.80
+
 
 module.exports = {
     _RAW_PROJECT_ID_, 
     _MART_PROJECT_ID_, 
-    _PRESENTATION_PROJECT_ID_
+    _PRESENTATION_PROJECT_ID_,
+    _GENDER_THRESHOLD_
 };
